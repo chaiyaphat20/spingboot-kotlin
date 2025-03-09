a292d748a8b007a7380bc385db8177aaf2d54a77
chaiyaphat supharak <chaiyaphat2024@gmail.com>
2025-03-09 17:12:45 +0700

    demo

--- src/main/kotlin/com/itgenius/springdemp/controllers/DemoController.kt
+++ src/main/kotlin/com/itgenius/springdemp/controllers/DemoController.kt
@@ -1,10 +1,7 @@
 package com.itgenius.springdemp.controllers
 
 import com.itgenius.springdemp.models.DemoData
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,9 +26,38 @@ class DemoController {
         );
     }
 
+    //Get By ID
     @GetMapping("/demodata/{id}")
     fun  getDemoDataById(@PathVariable id:Int): DemoData {
         return DemoData(id,"name","name@gmail","09844444","Thai")
+    }
+
+    //Get with optional parameter
+    @GetMapping("/demodata2/search")
+    fun getDemoDataWithOptionalParam(
+        @RequestParam(required = false, defaultValue = "") name: String
+    ): DemoData {
+        val userName = if (name.isEmpty()) "Guest" else name
+        return DemoData(1, userName, "name@gmail.com", "09844444", "Thai")
+    }
+
+
+    //POST Method
+    @PostMapping("/demodata")
+    fun createDemoData(@RequestBody demoData: DemoData): DemoData {
+        return  demoData
+    }
+
+    //PUT Method
+    @PutMapping("/demodata/{id}")
+    fun updateDemoData(@RequestBody demoData: DemoData,@PathVariable id:Int): DemoData {
+        return  demoData.copy(id=id)
+    }
+
+    //Delte Method
+    @DeleteMapping("demodata/{id}")
+    fun deleteDemoData(@PathVariable id:Int): String {
+        return  "Delete Id: ${id}"
     }
 
 }
